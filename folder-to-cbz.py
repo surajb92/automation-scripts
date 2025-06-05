feded77a7cbe3375ee7f24960e8694e39d23c706
Suraj <surajb92@gmail.com>
2025-06-05 21:31:18 +0530

    Recursion clear, Deletion clear, Proper file placement clear. Tentatively complete.

--- folder-to-cbz.py
+++ folder-to-cbz.py
@@ -7,14 +7,13 @@ EXTENSIONS=['.jpg','.JPG','.jpeg','.JPEG','.png','.PNG','.webp','.WEBP']
 def get_images(base, E):
     file_list={}
     for root,directories,filenames in os.walk(base):
-        print(root,directories,filenames)
-        # something can be done here to sort?
         for filename in filenames:
             if any(ext in filename for ext in E):
-                if root not in file_list:
-                    file_list[root]=[os.path.join(root,filename)]
+                r=os.path.normpath(root)
+                if r not in file_list:
+                    file_list[r]=[os.path.join(r,filename)]
                 else:
-                    file_list[root].append(os.path.join(root,filename))
+                    file_list[r].append(os.path.join(r,filename))
     return file_list
 
 parser = argparse.ArgumentParser(
@@ -24,32 +23,30 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument("path",help="Path of folder to archive")
-parser.add_argument("-d","--delete",action="store_true",default=False,help="Deletes the original folder after archiving is complete.")
-#parser.add_argument("-s","--single",action="store_true",default=False,help="Puts all images into a single file, regardless of sub-directories.")
+parser.add_argument("-d","--delete",action="store_true",default=False,
+help="Deletes the original files (and the folder if it's empty) after archiving is complete.")
 args=parser.parse_args()
 folder=args.path
 delete=args.delete
-#single=args.single
-#print(folder,delete)
 
-# image_list = get_images(folder,EXTENSIONS)
-image_list = dict(reversed(list(get_images(folder,EXTENSIONS).items())))
 # Reverse order because need to delete inner files first
+image_list = dict(reversed(list(get_images(folder,EXTENSIONS).items())))
 
-#print(*image_list,sep='\n')
-#for i in image_list:
-#    print(*image_list[i],sep='\n')
- 
-for i in image_list:
-    print(os.path.dirname(i))
-    foldr = os.path.basename(os.path.normpath(i))
-    with zipfile.ZipFile(f"{os.path.dirname(os.path.dirname(i))}/{foldr}.cbz",'w') as myzip:
-        for j in image_list[i]:
-            myzip.write(j,os.path.basename(os.path.normpath(j)))
-            if delete:
-                os.remove(j)
+def main():
+    for i in image_list:
+        folder_name = os.path.basename(i)
+        parent = os.path.dirname(i)
+        with zipfile.ZipFile(f"{parent}/{folder_name}.cbz",'w') as myzip:
+            for j in image_list[i]:
+                myzip.write(j,os.path.basename(os.path.normpath(j)))
+                if delete:
+                    os.remove(j)
+        fol = f"{parent}/{folder_name}"
+        if delete:
+            try:
+                os.rmdir(fol)
+            except OSError:
+                print("Directory "+fol+" is not empty, so not deleted.")
 
-# if non image formats found, set delete to false (maybe error msg)? or just delete the image files?
-
-# Note : In case of nested file structure, create archive of cbz files?
-
+if __name__=="__main__":
+    main()

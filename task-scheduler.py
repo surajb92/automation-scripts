4d13e0ce6583f806ff55b20b16b61e3a9945b580
Suraj <surajb92@gmail.com>
2025-08-12 13:45:06 +0530

    Implemented Add & Delete tasks

--- task-scheduler.py
+++ task-scheduler.py
@@ -1,73 +1,85 @@
 import sys
+import json
 from PyQt5 import QtWidgets, QtGui, QtCore
 
 class CalendarPopup(QtWidgets.QWidget):
-    def __init__(self, calendar, parent=None):
+    def __init__(self, parent=None):
         super().__init__(parent, QtCore.Qt.Popup | QtCore.Qt.FramelessWindowHint)
         #self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.calendar = QtWidgets.QCalendarWidget()
+        self.calendar.setGridVisible(True)
+        self.calendar.show()
+        self.calendar.setFocusPolicy(QtCore.Qt.StrongFocus)
         layout = QtWidgets.QVBoxLayout(self)
-        layout.addWidget(calendar)
+        layout.addWidget(self.calendar)
         layout.setContentsMargins(0, 0, 0, 0)
-        self.setFixedSize(300, 200)  # Match previous calendar size
-        self.setWindowFlags(QtCore.Qt.Tool | QtCore.Qt.FramelessWindowHint)
-        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.setFixedSize(300, 300)
+        self.setWindowFlags(QtCore.Qt.Popup | QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint) # Change to dialog or tool later to tests
+        #self.setFocusPolicy(QtCore.Qt.StrongFocus)
         self.tray_menu_open = False
-    def focusOutEvent(self, event):
+        self.calendar_focus = False
+    '''def focusOutEvent(self, event):
         # Don't hide menu unless mouse is clicked elsewhere, right click also exempted
+        print('event reason: ',event.reason())
         if event.reason() == QtCore.Qt.ActiveWindowFocusReason:
             # don't hide if context menu open
             if self.tray_menu_open:
                 self.tray_menu_open = False
             else:
+                self.calendar_focus = False
                 self.hide()
+        elif event.reason() in (QtCore.Qt.PopupFocusReason, QtCore.Qt.OtherFocusReason):
+            pass
+        elif event.reason() == 0 and not self.calendar_focus:
+            self.calendar_focus = True
         else:
-            print("hiding")
+            self.tray_menu_open = False
+            self.calendar_focus = False
             self.hide()
-        super().focusOutEvent(event)
+        super().focusOutEvent(event)'''
 
     def tray_menu(self):
-        if not self.tray_menu_open:
+        if not self.tray_menu_open and self.isVisible():
             self.tray_menu_open = True
-        print("Tray menu status: ",self.tray_menu_open)
-    def closeEvent(self, event):
-        print("Close event triggered")
-        event.accept()  # Allow closing, or use event.ignore() to prevent if needed
-        super().closeEvent(event)
+            print('will this come first?')
 
 class SchedulerApp:
     def __init__(self):
         # Create app, keep it running in bg even if window closed
         self.app = QtWidgets.QApplication(sys.argv)
         self.app.setQuitOnLastWindowClosed(False)
-
+        
+        self.root_widget = QtWidgets.QWidget()
+        #self.root_widget.show()
         # Calendar dialog
-        self.calendar = QtWidgets.QCalendarWidget()
-        self.calendar_popup = CalendarPopup(self.calendar)
-        self.calendar.setGridVisible(True)
-        self.calendar.show()
+        self.calendar_popup = CalendarPopup(self.root_widget) #self.calendar)
 
         # Wrap calendar in a QWidget for the menu
-        calendar_widget = QtWidgets.QWidget()
-        layout = QtWidgets.QVBoxLayout(calendar_widget)
-        layout.addWidget(self.calendar)
-        layout.setContentsMargins(0, 0, 0, 0)
+        #calendar_widget = QtWidgets.QWidget()
+        #layout = QtWidgets.QVBoxLayout(calendar_widget)
+        #layout.addWidget(self.calendar)
+        #layout.setContentsMargins(0, 0, 0, 0)
         
         # Create system tray icon
-        self.tray_icon = QtWidgets.QSystemTrayIcon()
+        self.tray_icon = QtWidgets.QSystemTrayIcon(self.root_widget)
         self.tray_icon.setIcon(QtGui.QIcon.fromTheme("dialog-information"))
         self.tray_icon.setToolTip("Task Scheduler")
         self.tray_icon.show()
         
         # Right-click context menu
-        self.tray_menu = QtWidgets.QMenu()
+        self.tray_menu = QtWidgets.QMenu(self.root_widget)
         self.quit_action = self.tray_menu.addAction("Quit")
         self.tray_icon.setContextMenu(self.tray_menu)
-        
+                
         # Connect actions to functions
         self.tray_icon.activated.connect(self.handle_tray_click)
         self.tray_menu.aboutToShow.connect(self.calendar_popup.tray_menu)
         #self.tray_menu.aboutToHide.connect(self.calendar_popup.tray_menu)
         self.quit_action.triggered.connect(self.quit_app)
+        self.calendar_popup.calendar.clicked.connect(self.task_manager) #add_task)
+
+        # Focus handler
+        self.app.focusObjectChanged.connect(lambda old=None,new=None: self.focus_handler(old,new)) # This lambda solution is as amazing as ever lol
 
         # Notification when app is launched
         self.tray_icon.showMessage(
@@ -76,21 +88,165 @@ class SchedulerApp:
             QtGui.QIcon.fromTheme("dialog-information"),
             3000
         )
+
+        # Initialize task storage
+        self.tasks = self.load_tasks()
+        
+        # Highlight format for dates with tasks
+        self.task_format = QtGui.QTextCharFormat()
+        self.task_format.setBackground(QtGui.QColor("lightgreen"))
+        self.task_format.setFontWeight(75)  # Bold
+
+        # Highlight existing tasks
+        self.highlight_task_dates()
+
+    # Load tasks from JSON file
+    def load_tasks(self):
+        try:
+            with open("tasks.json", "r") as file:
+                data = json.load(file)
+                return {
+                    QtCore.QDate.fromString(date, "yyyy-MM-dd"): tasks
+                    for date, tasks in data.items()
+                }
+        except FileNotFoundError:
+            return {}
+    
+    # Save tasks to JSON file.
+    def save_tasks(self):
+        data = {date.toString("yyyy-MM-dd"): tasks for date, tasks in self.tasks.items()}
+        with open("tasks.json", "w") as file:
+            json.dump(data, file, indent=4)
+    
+    # Task manager window
+    def task_manager(self,date):
+        # self.central_widget = QWidget()
+        # self.setCentralWidget(self.central_widget)
+        self.calendar_popup.hide()
+        self.task_date = date
+        
+        # RESUME
+        #central_widget = QtWidgets.QWidget()
+        #self.app.setCentralWidget(central_widget)
+        layout0 = QtWidgets.QHBoxLayout()
+        layout1 = QtWidgets.QVBoxLayout()
+        layout2 = QtWidgets.QVBoxLayout()
+        
+        self.task_popup = QtWidgets.QWidget()
+        self.task_popup.setLayout(layout0)
+        
+        task_list_entries = QtWidgets.QWidget(self.task_popup)
+        task_list_buttons = QtWidgets.QWidget(self.task_popup)
+        
+        self.task_list = QtWidgets.QListWidget(task_list_entries)
+        self.task_input = QtWidgets.QLineEdit(task_list_entries)
+        self.task_input.setPlaceholderText("Enter a new task")
+        self.add_button = QtWidgets.QPushButton("Add Task", task_list_buttons)
+        self.edit_button = QtWidgets.QPushButton("Edit Task", task_list_buttons)
+        self.delete_button = QtWidgets.QPushButton("Delete Task", task_list_buttons)
+        self.add_button.setShortcut("Enter")
+        
+        self.add_button.clicked.connect(self.add_task)
+        self.delete_button.clicked.connect(self.delete_task)
+        
+        self.update_task_list()
+        
+        layout0.addWidget(task_list_entries)
+        layout0.addWidget(task_list_buttons)
+        
+        layout1.addWidget(self.task_list)
+        layout1.addWidget(self.task_input)
+        
+        layout2.addWidget(self.delete_button)
+        layout2.addWidget(self.edit_button)
+        layout2.addWidget(self.add_button)
+        
+        task_list_entries.setLayout(layout1)
+        task_list_buttons.setLayout(layout2)
+        
+        self.task_popup.show()
+    
+    # Add a task.
+    def add_task(self):
+        t = self.task_input.text()
+        d = self.task_date
+        if d in self.tasks:
+            if t in self.tasks[d]:
+                m = QtWidgets.QMessageBox()
+                m.setWindowTitle("Warning!")
+                m.setIcon(QtWidgets.QMessageBox.Information)
+                m.setText("Task already exists!")
+                m.exec()
+                return
+            else:
+                self.tasks[d].append(t)
+        else:
+            self.tasks[d] = [t]
+        self.task_input.clear()
+        self.update_task_list()
+        self.save_tasks()
+    
+    # Update list of tasks when modified
+    def update_task_list(self):
+        self.task_list.clear()
+        d = self.task_date
+        if self.tasks.get(d):
+            for t in self.tasks[d]:
+                self.task_list.addItem(t)
+    
+    # Delete the selected task
+    def delete_task(self):
+        if not self.task_list.selectedItems():
+            return
+        t = self.task_list.selectedItems()[0].text()
+        m = QtWidgets.QMessageBox()
+        m.setWindowTitle("Warning!")
+        m.setIcon(QtWidgets.QMessageBox.Warning)
+        m.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+        m.setText("Are you sure you want to delete the task \'"+t+"\'?")
+        response = m.exec()
+        if response == QtWidgets.QMessageBox.Yes:
+            self.tasks[self.task_date].remove(t)
+            self.save_tasks()
+            self.update_task_list()
+        else:
+            return
+    
+    # Highlight dates with tasks on the calendar.
+    def highlight_task_dates(self):
+        for date in self.tasks.keys():
+            self.calendar_popup.calendar.setDateTextFormat(date, self.task_format)
+    
+    # Function for when user left clicks tray icon
     def handle_tray_click(self, reason):
         if reason == QtWidgets.QSystemTrayIcon.Trigger : # i.e. if left-clicked
-            tray_pos = self.tray_icon.geometry().topLeft()
+            #tray_pos = self.tray_icon.geometry().topLeft()
             #screen = QtWidgets.QApplication.primaryScreen().availableGeometry()
             #tray_pos = screen.bottomRight() - QtGui.QCursor.pos() # 
             if self.calendar_popup.isVisible():
                 self.calendar_popup.hide()
             else:
-                tray_pos = QtGui.QCursor.pos() + QtCore.QPoint(10, 10)
+                tray_pos = QtGui.QCursor.pos() # 
+                tray_pos = QtCore.QPoint(QtWidgets.QWidget.mapToGlobal(1900, 800))
                 self.calendar_popup.move(tray_pos)
                 self.calendar_popup.show()
-                self.calendar_popup.raise_()
-                self.calendar_popup.setFocus()
-                print(f"Popup shown at {tray_pos.x()},{tray_pos.y()}")
-    
+                #self.calendar_popup.raise_()
+                #self.calendar_popup.setFocus()
+                #self.calendar_popup.calendar.setFocus()
+                print(f"Popup shown at {self.calendar_popup.x()},{self.calendar_popup.y()}")
+        else:
+            print("do we get here for right clicks?")
+                
+    def focus_handler(self,old,new):
+        # print("Focus: ",old," --> ",new) 
+        if (new == None):
+            if self.calendar_popup.tray_menu_open:
+                self.calendar_popup.tray_menu_open = False
+            else:
+                pass
+                #self.calendar_popup.hide()
+        #if old == self.calendar_popup:
+
     def quit_app(self):
         self.tray_icon.hide()
         self.app.quit()
